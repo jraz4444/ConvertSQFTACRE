b4b846ec38d6d81acc59beb67419fc120bbd7ac7
jraz4444 <105386070+jraz4444@users.noreply.github.com>
2024-08-08 18:14:12 -0400

    Create Ft^2ACRE.py

---
+++ Ft^2ACRE.py
@@ -0,0 +1,8 @@
+#Land Calculation
+# One acre of land is equal to 43,560 sqaure feet.
+# Ask the user to enter the total square feet in a tract of land
+tractLand = int(input("Enter the total square feet in a tract of land"))
+
+#Calculate the number of acres in the tract
+numberAcres = tractLand / 43560
+print(numberAcres)
